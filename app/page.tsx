e3491362fc7257c7ca9924e85ee3585cbade0d80
Alexander Frost <alexandr.frost1@gmail.com>
2025-02-03 09:33:59 +1100

    changed descript

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,11 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+       Alex Frost
       </h1>
       <h2 className="mb-4 text-xl font-medium">Electrical Engineer</h2>
       <p className="mb-4">
-        {`I am a final-year Electrical Engineering student at the University of Wollongong, 1 part-time semester away from having completed all my coursework, and I am eager to apply my technical knowledge and problem-solving skills in a practical environment. With a solid foundation in engineering, a passion for problem-solving, and experience in drafting technical diagrams and liaising with vendors professionally, I am confident I can add significant value to the company.
-`}
+        {`I am Alex Frost, a final-year Electrical Engineering student at the University of Wollongong.`}
       </p>
       <div className="my-8">
         <BlogPosts />
