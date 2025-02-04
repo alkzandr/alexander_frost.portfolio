eeb86bb5a8b207ec7ba88cc94767dda204ac4333
Alexander Frost <alexandr.frost1@gmail.com>
2025-02-04 16:51:27 +1100

    added tailwind and tried fixing dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class', // Use 'class' for manual dark mode control
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',  // Include paths to all files where you use Tailwind CSS
+    './components/**/*.{js,ts,jsx,tsx}',  // Include component paths too
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
