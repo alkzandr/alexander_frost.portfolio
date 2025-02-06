94ee85188017d9a1b811f9a80d3c4038e5ac4c8f
Alexander Frost <alexandr.frost1@gmail.com>
2025-02-06 14:19:44 +1100

    changed tailwind in root to disable all dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  darkMode: 'class', // Use 'class' for manual dark mode control
+  darkMode: false, // Use 'class' for manual dark mode control
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',  // Include paths to all files where you use Tailwind CSS
     './components/**/*.{js,ts,jsx,tsx}',  // Include component paths too
