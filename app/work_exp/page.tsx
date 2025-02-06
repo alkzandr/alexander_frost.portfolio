76ce98883590c0c4a61a6eedfde4d348bf278dbd
Alexander Frost <alexandr.frost1@gmail.com>
2025-02-06 13:52:21 +1100

    changed text colour

--- app/work_exp/page.tsx
+++ app/work_exp/page.tsx
@@ -10,7 +10,7 @@ export default function WorkExperience() {
         <h2 className="text-2xl font-semibold text-white">Previous Jobs</h2>
         <div className="mt-6 space-y-4">
           <div className="p-4 bg-gray-100 rounded-lg shadow-sm">
-            <h3 className="text-xl font-bold text-gray-800">Draftsperson | Molenaar x McNeice</h3>
+            <h3 className="text-xl font-bold text-gray-900">Draftsperson | Molenaar x McNeice</h3>
             <p className="text-sm text-gray-600">June 2021 - Current</p>
             <p className="mt-2 text-gray-700">
               Assisted with the creation of technical diagrams and blueprints, ensuring accuracy and clarity in the drafting process for electrical systems, as well as extensively using communication skills to liaise with vendors and develop strong workplace relationships.
