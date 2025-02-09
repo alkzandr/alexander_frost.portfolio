f986263ac1d7c44ea5f2391b0b9e7cf354bd69f6
Alexander Frost <alexandr.frost1@gmail.com>
2025-02-10 10:34:53 +1100

    added logo no light mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,22 @@
-"use client"
-
-import Image from "next/image"
-import { BlogPosts } from "app/components/posts"
+import { BlogPosts } from 'app/components/posts'
 
 export default function Page() {
   return (
     <section>
-      <div className="flex items-center mb-4">
-        {/* Light mode logo (default) */}
+        <div className="flex items-center mb-4">
         <Image
-          src="/logo_dark.png"
+          src="/images/logo_light.png"
           alt="Alex Frost Logo"
           width={40}
           height={40}
-          className="mr-4 block dark:hidden"
-        />
-        {/* Dark mode logo */}
-        <Image
-          src="/logo_light.png"
-          alt="Alex Frost Logo"
-          width={40}
-          height={40}
-          className="mr-4 hidden dark:block"
+          className="mr-4"
         />
         <div>
-          <h1 className="mb-1 text-2xl font-semibold tracking-tighter">
-            Alex Frost
-          </h1>
-          <h2 className="mb-1 text-xl font-medium">Electrical Engineer</h2>
-          <h3 className="mb-8 text-lg font-normal text-gray-600">Wollongong, NSW</h3>
-        </div>
-      </div>
+      <h1 className="mb-1 text-2xl font-semibold tracking-tighter">
+       Alex Frost
+      </h1>
+      <h2 className="mb-1 text-xl font-medium">Electrical Engineer</h2>
+      <h3 className="mb-8 text-lg font-normal text-gray-600">Wollongong, NSW</h3>
       <p className="mb-4">
         {`This is a showcase of various academic and personal projects I have completed throughout my career`}
       </p>
